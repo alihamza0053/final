cc009d457caa5e8a7a06d74601750378afe8d9c6
Ali Hamza <alihamza00053@gmail.com>
2023-06-05 12:50:15 +0500

    Update Support.kt

--- final_year_project/app/src/main/java/com/digital/yazman/ah/Support.kt
+++ final_year_project/app/src/main/java/com/digital/yazman/ah/Support.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -15,15 +18,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.digital.yazman.ah.ui.theme.DigitalYazmanTheme
 
 class Support : ComponentActivity() {
@@ -43,6 +52,20 @@ class Support : ComponentActivity() {
                 Font(R.font.lexend_thin, FontWeight.Thin),
             )
             val context = LocalContext.current
+            var white_text by remember {
+                mutableStateOf(Color(0xFFFFFFFF))
+            }
+            var white_bg by remember {
+                mutableStateOf(Color(0xFFFFFFFF))
+            }
+            var purple_bg by remember {
+                mutableStateOf(Color(0xFF800080))
+            }
+
+            var black_text by remember {
+                mutableStateOf(Color(0xFF000000))
+            }
+
             DigitalYazmanTheme {
                 Column(
                     modifier = Modifier
@@ -55,21 +78,59 @@ class Support : ComponentActivity() {
                     //business start
                     Text(
                         text = "Support",
-                        color = Color(0xFF000000),
+                        color = Color(0xFFFFFFFF),
                         fontSize = 25.sp,
                         fontFamily = fontFamily,
                         fontWeight = FontWeight.SemiBold,
                         modifier = Modifier.padding(top = 15.dp, start = 20.dp)
                     )
                     Row(
-                        modifier = Modifier
-                            .background(Color.White),
+                        modifier = Modifier.padding(5.dp),
                     ) {
+                        Spacer(modifier = Modifier.padding(10.dp))
+
                         Text(
                             text = "FAQ",
-                        )
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.Center,
+                            fontFamily = fontFamily,
+                            color = white_text,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier
+                                .background(purple_bg)
+                                .padding(10.dp)
+                                .weight(1f)
+                                .clickable {
+                                    black_text = Color(0xFF000000)
+                                    white_text = Color(0xFFFFFFFF)
+                                    purple_bg = Color(0xFF800080)
+                                    white_bg = Color(0xFFFFFFFF)
 
-                        Text(text = "Contact")
+                                }
+                        )
+                        Spacer(modifier = Modifier.padding(10.dp))
+
+                        Text(
+                            text = "Contact",
+                            fontSize = 20.sp,
+                            textAlign = TextAlign.Center,
+                            fontFamily = fontFamily,
+                            color = black_text,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier
+                                .background(white_bg)
+                                .padding(10.dp)
+                                .weight(1f)
+                                .clickable {
+                                    black_text = Color(0xFFFFFFFF)
+                                    white_text = Color(0xFF000000)
+                                    purple_bg = Color(0xFFFFFFFF)
+                                    white_bg = Color(0xFF800080)
+
+                                }
+                        )
+                        Spacer(modifier = Modifier.padding(10.dp))
+
 
                     }
                 }
